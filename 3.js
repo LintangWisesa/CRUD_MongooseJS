5b63a64ccbb63bbeda6080bce73e12b809f63e45
lintangwisesa <lintangwisesa@ymail.com>
2019-01-25 10:49:55 +0700

    mongoose

---
+++ 3.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose')
+var Produk = require('./models/produk')
+var url = 
+'mongodb://lintang:1234@localhost:27017/db_mongoose'
+
+mongoose.connect(url, ()=>{
+    console.log('Terhubung ke MongoDB')
+})
+
+Produk.find({
+    nama: 'Kemeja Batik'
+}, (error, data)=>{
+    console.log(data)
+})
